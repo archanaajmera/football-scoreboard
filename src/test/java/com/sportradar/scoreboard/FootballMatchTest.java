b56714795e9a8548ac1c0bc71a609317e7da89a0
archanaajmera <30ajmera.archana@gmail.com>
2023-10-04 22:57:06 +0200

    Initial draft

---
+++ src/test/java/com/sportradar/scoreboard/FootballMatchTest.java
@@ -0,0 +1,14 @@
+package com.sportradar.scoreboard;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FootballMatchTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/sportradar/scoreboard/ScoreBoardTest.java
@@ -0,0 +1,14 @@
+package com.sportradar.scoreboard;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ScoreBoardTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
