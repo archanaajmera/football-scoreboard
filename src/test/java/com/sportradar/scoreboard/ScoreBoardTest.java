248a3c0ddc187d3d9551c903f2f4d81f16837c37
archanaajmera <30ajmera.archana@gmail.com>
2023-10-05 14:33:47 +0200

    add more test for startGame method with the same team

--- src/test/java/com/sportradar/scoreboard/ScoreBoardTest.java
+++ src/test/java/com/sportradar/scoreboard/ScoreBoardTest.java
@@ -2,6 +2,8 @@ package com.sportradar.scoreboard;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,7 +20,29 @@ public class ScoreBoardTest {
     public void testStartGame() {
         scoreBoard.startGame("Mexico", "Canada");
         assertEquals(1, scoreBoard.getSummary().size());
-		
     }
+	
+	@Test
+	public void testStartGameInitialScore() {
+	    scoreBoard.startGame("Mexico", "Canada");
+
+	    List<FootballMatch> summary = scoreBoard.getSummary();
+	    assertEquals("Mexico 0 - Canada 0", summary.get(0).toString());
+	}
+	
+	@Test
+    public void testStartGameWithSameTeams() {
+        scoreBoard.startGame("Mexico", "Mexico");
+        assertEquals(0, scoreBoard.getSummary().size());
+    }
+	
+	@Test
+    public void testStartGameWithSameTeamsAgain() {
+        scoreBoard.startGame("Mexico", "Canada");
+        scoreBoard.startGame("Mexico", "Canada");
+        
+        assertEquals(1, scoreBoard.getSummary().size());
+    }
+
 	
 }
