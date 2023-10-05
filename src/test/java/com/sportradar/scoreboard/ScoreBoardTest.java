07a783100e30c5fb7ee47eaac4d9304f4c97a47e
archanaajmera <30ajmera.archana@gmail.com>
2023-10-05 17:30:59 +0200

    add tests for empty, null, whitespaces and case sensitive team names

--- src/test/java/com/sportradar/scoreboard/ScoreBoardTest.java
+++ src/test/java/com/sportradar/scoreboard/ScoreBoardTest.java
@@ -31,6 +31,14 @@ public class ScoreBoardTest {
 	}
 	
 	@Test
+    public void testStartMultipleGames() {
+        scoreBoard.startGame("Mexico", "Canada");
+        scoreBoard.startGame("Spain", "Brazil");
+
+        assertEquals(2, scoreBoard.getSummary().size());
+    }
+	
+	@Test
     public void testStartGameWithSameTeams() {
         scoreBoard.startGame("Mexico", "Mexico");
         assertEquals(0, scoreBoard.getSummary().size());
@@ -41,6 +49,45 @@ public class ScoreBoardTest {
         scoreBoard.startGame("Mexico", "Canada");
         scoreBoard.startGame("Mexico", "Canada");
         
+        assertEquals(1, scoreBoard.getSummary().size());
+    }
+	
+	@Test(expected = IllegalArgumentException.class)
+    public void testStartGameWithEmptyTeamNames() {
+        scoreBoard.startGame("", "Canada");
+        scoreBoard.startGame("Mexico", "");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testStartGameWithNullTeamNames() {
+        scoreBoard.startGame(null, "Canada");
+        scoreBoard.startGame("Mexico", null);
+    }
+    
+    @Test
+    public void testStartGameWithWhitespaceInTeamNames() {
+        scoreBoard.startGame("  Mexico  ", "  Canada  ");
+
+        List<FootballMatch> summary = scoreBoard.getSummary();
+        assertEquals(1, summary.size());
+
+        FootballMatch match = summary.get(0);
+        assertEquals("Mexico", match.getHomeTeam());
+        assertEquals("Canada", match.getAwayTeam());
+    }
+    
+    @Test
+    public void testCaseInsensitiveTeamNames() {
+    	
+        scoreBoard.startGame("MEXIco", "canada");
+        
+        try {
+            scoreBoard.startGame("mexico", "CANADA");
+            fail("Expected IllegalArgumentException to be thrown.");
+        } catch (IllegalArgumentException e) {
+            assertEquals("A game with the same teams already exists.", e.getMessage());
+        }
+
         assertEquals(1, scoreBoard.getSummary().size());
     }
 
