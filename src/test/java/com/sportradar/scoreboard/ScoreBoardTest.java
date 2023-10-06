56fffe4a77096b5a491c3ac383f16cf7685fb0d5
archanaajmera <30ajmera.archana@gmail.com>
2023-10-06 12:14:38 +0200

    add more test for update score of a game

--- src/test/java/com/sportradar/scoreboard/ScoreBoardTest.java
+++ src/test/java/com/sportradar/scoreboard/ScoreBoardTest.java
@@ -147,4 +147,61 @@ public class ScoreBoardTest {
         assertEquals(2, match.getAwayScore());
     }
     
+    @Test
+    public void testUpdateScoreWithMultipleGames() {
+    	scoreBoard.startGame("Mexico", "Canada");
+        scoreBoard.startGame("Spain", "Brazil");
+        scoreBoard.startGame("Germany", "France");
+
+        scoreBoard.updateScore("Spain", "Brazil", 10, 2);
+
+        List<FootballMatch> summary = scoreBoard.getSummary();
+        assertEquals(3, summary.size());
+
+        for (FootballMatch match : summary) {
+            if (match.getHomeTeam().equals("Spain") && match.getAwayTeam().equals("Brazil")) {
+            	assertEquals(10, match.getHomeScore());
+                assertEquals(2, match.getAwayScore());
+            }else {
+            	 assertEquals(0, match.getHomeScore());
+                 assertEquals(0, match.getAwayScore());
+            }
+        } 
+    }
+    
+    @Test
+    public void testUpdateScoreForMultipleMatches() {
+        scoreBoard.startGame("Mexico", "Canada");
+        scoreBoard.startGame("Spain", "Brazil");
+        scoreBoard.updateScore("Mexico", "Canada", 0, 5);
+        scoreBoard.updateScore("Spain", "Brazil", 10, 2);
+        assertEquals(0, scoreBoard.getSummary().get(0).getHomeScore());
+        assertEquals(5, scoreBoard.getSummary().get(0).getAwayScore());
+        assertEquals(10, scoreBoard.getSummary().get(1).getHomeScore());
+        assertEquals(2, scoreBoard.getSummary().get(1).getAwayScore());
+    }
+
+    @Test
+    public void testUpdateScoreWithNegativeScores() {
+        scoreBoard.startGame("Spain", "Brazil");
+        try {
+            scoreBoard.updateScore("Spain", "Brazil", -1, 2);
+            fail("Expected IllegalArgumentException was not thrown.");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Scores cannot be negative.", e.getMessage());
+        }
+    }
+    
+    @Test
+    public void testUpdateScoreForNonExistingMatch() {
+        try {
+            scoreBoard.updateScore("Spain", "Brazil", 2, 1);
+            fail("Expected IllegalArgumentException was not thrown.");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Match does not exist for the provided teams.", e.getMessage());
+        }
+    }
+    
+      
+   
 }
